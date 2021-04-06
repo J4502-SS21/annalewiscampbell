45b2baeeece74a31ba4cae46494c8d7cbb89ed71
AnnaLewisCampbell <77590020+AnnaLewisCampbell@users.noreply.github.com>
2021-04-06 10:29:52 -0500

    Hamburger menu

--- main.js
+++ main.js
@@ -2,5 +2,13 @@
 
 var menuNav = document.getElementById("menu_nav");
 
-console.log("Hello, World!");
-alert("Hello, World");
+var menuTrigger = document.getElementById('menu_trigger')
+
+menuTrigger.onclick = function() {
+  if (menuNav.style.display === "flex") {
+    menuNav.style.display = "none";
+  } else {
+    menuNav.style.display = "flex";
+  }
+  console.log("The menu has been toggled")
+}
